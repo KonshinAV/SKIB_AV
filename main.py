ca3ff7a6569544f4eec1fc3194832522193b086f
konshin.av (virtual) <konshinav88@gmail.com>
2020-10-06 11:57:31 +0100

    Add check 192.168.31.1:445

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from utils import secure_string
+from utils.nettools import test_tcp_port_connection
 
 if __name__ == '__main__':
     pwd = secure_string.SecureString("Pass")
     print (pwd.encoding())
+    print (test_tcp_port_connection('192.168.31.1',445))
     pass
